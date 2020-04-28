8d4a531fa9c1eb374769e945c3f0cd1ba619967e
Amalie Snebæk Christensen <amaliesnebaek@live.dk>
2020-04-28 10:13:06 +0200

    Opgave 1 design tf_,mat

--- Eksamensprojekt - Matematikside/Eksamensprojekt - Matematikside/Trigonomiske funktioner.Designer.vb
+++ Eksamensprojekt - Matematikside/Eksamensprojekt - Matematikside/Trigonomiske funktioner.Designer.vb
@@ -27,6 +27,17 @@ Partial Class Trigonomiske_funktioner
         Me.Pbox_Tilbage_TrigonomiskeFunktioner = New System.Windows.Forms.PictureBox()
         Me.Lbl_TFOpgave1 = New System.Windows.Forms.Label()
         Me.Lbl_opg1a = New System.Windows.Forms.Label()
+        Me.Lbl_opg1b = New System.Windows.Forms.Label()
+        Me.Lbl_opg1c = New System.Windows.Forms.Label()
+        Me.Lbl_opg1d = New System.Windows.Forms.Label()
+        Me.Txtbox_opg1a = New System.Windows.Forms.TextBox()
+        Me.Txtbox_opg1b = New System.Windows.Forms.TextBox()
+        Me.Txtbox_opg1c = New System.Windows.Forms.TextBox()
+        Me.Txtbox_opg1d = New System.Windows.Forms.TextBox()
+        Me.But_opg1a_tjek = New System.Windows.Forms.Button()
+        Me.But_opg1b_tjek = New System.Windows.Forms.Button()
+        Me.But_opg1c_tjek = New System.Windows.Forms.Button()
+        Me.But_opg1d_tjek = New System.Windows.Forms.Button()
         CType(Me.Pbox_Tilbage_TrigonomiskeFunktioner, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -65,17 +76,126 @@ Partial Class Trigonomiske_funktioner
         '
         Me.Lbl_opg1a.AutoSize = True
         Me.Lbl_opg1a.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Lbl_opg1a.Location = New System.Drawing.Point(64, 194)
+        Me.Lbl_opg1a.Location = New System.Drawing.Point(64, 190)
         Me.Lbl_opg1a.Name = "Lbl_opg1a"
         Me.Lbl_opg1a.Size = New System.Drawing.Size(19, 16)
         Me.Lbl_opg1a.TabIndex = 6
         Me.Lbl_opg1a.Text = "a)"
+        '
+        'Lbl_opg1b
+        '
+        Me.Lbl_opg1b.AutoSize = True
+        Me.Lbl_opg1b.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg1b.Location = New System.Drawing.Point(64, 222)
+        Me.Lbl_opg1b.Name = "Lbl_opg1b"
+        Me.Lbl_opg1b.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg1b.TabIndex = 7
+        Me.Lbl_opg1b.Text = "b)"
+        '
+        'Lbl_opg1c
+        '
+        Me.Lbl_opg1c.AutoSize = True
+        Me.Lbl_opg1c.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg1c.Location = New System.Drawing.Point(64, 251)
+        Me.Lbl_opg1c.Name = "Lbl_opg1c"
+        Me.Lbl_opg1c.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg1c.TabIndex = 8
+        Me.Lbl_opg1c.Text = "c)"
+        '
+        'Lbl_opg1d
+        '
+        Me.Lbl_opg1d.AutoSize = True
+        Me.Lbl_opg1d.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg1d.Location = New System.Drawing.Point(64, 281)
+        Me.Lbl_opg1d.Name = "Lbl_opg1d"
+        Me.Lbl_opg1d.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg1d.TabIndex = 9
+        Me.Lbl_opg1d.Text = "d)"
+        '
+        'Txtbox_opg1a
+        '
+        Me.Txtbox_opg1a.Location = New System.Drawing.Point(90, 190)
+        Me.Txtbox_opg1a.Name = "Txtbox_opg1a"
+        Me.Txtbox_opg1a.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg1a.TabIndex = 10
+        '
+        'Txtbox_opg1b
+        '
+        Me.Txtbox_opg1b.Location = New System.Drawing.Point(89, 222)
+        Me.Txtbox_opg1b.Name = "Txtbox_opg1b"
+        Me.Txtbox_opg1b.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg1b.TabIndex = 11
+        '
+        'Txtbox_opg1c
+        '
+        Me.Txtbox_opg1c.Location = New System.Drawing.Point(89, 251)
+        Me.Txtbox_opg1c.Name = "Txtbox_opg1c"
+        Me.Txtbox_opg1c.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg1c.TabIndex = 12
+        '
+        'Txtbox_opg1d
+        '
+        Me.Txtbox_opg1d.Location = New System.Drawing.Point(89, 281)
+        Me.Txtbox_opg1d.Name = "Txtbox_opg1d"
+        Me.Txtbox_opg1d.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg1d.TabIndex = 13
+        '
+        'But_opg1a_tjek
+        '
+        Me.But_opg1a_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg1a_tjek.Location = New System.Drawing.Point(209, 190)
+        Me.But_opg1a_tjek.Name = "But_opg1a_tjek"
+        Me.But_opg1a_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg1a_tjek.TabIndex = 14
+        Me.But_opg1a_tjek.Text = "Tjek"
+        Me.But_opg1a_tjek.UseVisualStyleBackColor = True
+        '
+        'But_opg1b_tjek
+        '
+        Me.But_opg1b_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg1b_tjek.Location = New System.Drawing.Point(209, 222)
+        Me.But_opg1b_tjek.Name = "But_opg1b_tjek"
+        Me.But_opg1b_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg1b_tjek.TabIndex = 15
+        Me.But_opg1b_tjek.Text = "Tjek"
+        Me.But_opg1b_tjek.UseVisualStyleBackColor = True
+        '
+        'But_opg1c_tjek
+        '
+        Me.But_opg1c_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg1c_tjek.Location = New System.Drawing.Point(209, 250)
+        Me.But_opg1c_tjek.Name = "But_opg1c_tjek"
+        Me.But_opg1c_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg1c_tjek.TabIndex = 16
+        Me.But_opg1c_tjek.Text = "Tjek"
+        Me.But_opg1c_tjek.UseVisualStyleBackColor = True
+        '
+        'But_opg1d_tjek
+        '
+        Me.But_opg1d_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg1d_tjek.Location = New System.Drawing.Point(209, 281)
+        Me.But_opg1d_tjek.Name = "But_opg1d_tjek"
+        Me.But_opg1d_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg1d_tjek.TabIndex = 17
+        Me.But_opg1d_tjek.Text = "Tjek"
+        Me.But_opg1d_tjek.UseVisualStyleBackColor = True
         '
         'Trigonomiske_funktioner
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(816, 447)
+        Me.Controls.Add(Me.But_opg1d_tjek)
+        Me.Controls.Add(Me.But_opg1c_tjek)
+        Me.Controls.Add(Me.But_opg1b_tjek)
+        Me.Controls.Add(Me.But_opg1a_tjek)
+        Me.Controls.Add(Me.Txtbox_opg1d)
+        Me.Controls.Add(Me.Txtbox_opg1c)
+        Me.Controls.Add(Me.Txtbox_opg1b)
+        Me.Controls.Add(Me.Txtbox_opg1a)
+        Me.Controls.Add(Me.Lbl_opg1d)
+        Me.Controls.Add(Me.Lbl_opg1c)
+        Me.Controls.Add(Me.Lbl_opg1b)
         Me.Controls.Add(Me.Lbl_opg1a)
         Me.Controls.Add(Me.Lbl_TFOpgave1)
         Me.Controls.Add(Me.Pbox_Tilbage_TrigonomiskeFunktioner)
@@ -92,4 +212,15 @@ Partial Class Trigonomiske_funktioner
     Friend WithEvents Pbox_Tilbage_TrigonomiskeFunktioner As PictureBox
     Friend WithEvents Lbl_TFOpgave1 As Label
     Friend WithEvents Lbl_opg1a As Label
+    Friend WithEvents Lbl_opg1b As Label
+    Friend WithEvents Lbl_opg1c As Label
+    Friend WithEvents Lbl_opg1d As Label
+    Friend WithEvents Txtbox_opg1a As TextBox
+    Friend WithEvents Txtbox_opg1b As TextBox
+    Friend WithEvents Txtbox_opg1c As TextBox
+    Friend WithEvents Txtbox_opg1d As TextBox
+    Friend WithEvents But_opg1a_tjek As Button
+    Friend WithEvents But_opg1b_tjek As Button
+    Friend WithEvents But_opg1c_tjek As Button
+    Friend WithEvents But_opg1d_tjek As Button
 End Class
