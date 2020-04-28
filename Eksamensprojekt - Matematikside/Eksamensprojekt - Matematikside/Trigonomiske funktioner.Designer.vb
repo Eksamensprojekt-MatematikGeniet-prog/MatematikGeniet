9e14542542e094876822304c94e4d41483877558
Amalie Snebæk Christensen <amaliesnebaek@live.dk>
2020-04-28 10:26:16 +0200

    Opgave 1 og 2 tf_mat

--- Eksamensprojekt - Matematikside/Eksamensprojekt - Matematikside/Trigonomiske funktioner.Designer.vb
+++ Eksamensprojekt - Matematikside/Eksamensprojekt - Matematikside/Trigonomiske funktioner.Designer.vb
@@ -38,6 +38,19 @@ Partial Class Trigonomiske_funktioner
         Me.But_opg1b_tjek = New System.Windows.Forms.Button()
         Me.But_opg1c_tjek = New System.Windows.Forms.Button()
         Me.But_opg1d_tjek = New System.Windows.Forms.Button()
+        Me.Lbl_TFOpgave2 = New System.Windows.Forms.Label()
+        Me.But_opg2d_tjek = New System.Windows.Forms.Button()
+        Me.But_opg2c_tjek = New System.Windows.Forms.Button()
+        Me.But_opg2b_tjek = New System.Windows.Forms.Button()
+        Me.But_opg2a_tjek = New System.Windows.Forms.Button()
+        Me.Txtbox_opg2d = New System.Windows.Forms.TextBox()
+        Me.Txtbox_opg2c = New System.Windows.Forms.TextBox()
+        Me.Txtbox_opg2b = New System.Windows.Forms.TextBox()
+        Me.Txtbox_opg2a = New System.Windows.Forms.TextBox()
+        Me.Lbl_opg2d = New System.Windows.Forms.Label()
+        Me.Lbl_opg2c = New System.Windows.Forms.Label()
+        Me.Lbl_opg2b = New System.Windows.Forms.Label()
+        Me.Lbl_opg2a = New System.Windows.Forms.Label()
         CType(Me.Pbox_Tilbage_TrigonomiskeFunktioner, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -67,10 +80,10 @@ Partial Class Trigonomiske_funktioner
         Me.Lbl_TFOpgave1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Lbl_TFOpgave1.Location = New System.Drawing.Point(64, 80)
         Me.Lbl_TFOpgave1.Name = "Lbl_TFOpgave1"
-        Me.Lbl_TFOpgave1.Size = New System.Drawing.Size(236, 96)
+        Me.Lbl_TFOpgave1.Size = New System.Drawing.Size(240, 96)
         Me.Lbl_TFOpgave1.TabIndex = 5
-        Me.Lbl_TFOpgave1.Text = "Opgave 1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Følgende vinkelmål skal regnes i grader" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "a) 1 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "b) 2,3468 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "c) 4," &
-    "0456 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "d) 5,9732" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Lbl_TFOpgave1.Text = "Opgave 1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Følgende vinkelmål skal regnes i grader:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "a) 1 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "b) 2,3468 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "c) 4" &
+    ",0456 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "d) 5,9732 rad" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'Lbl_opg1a
         '
@@ -180,11 +193,143 @@ Partial Class Trigonomiske_funktioner
         Me.But_opg1d_tjek.Text = "Tjek"
         Me.But_opg1d_tjek.UseVisualStyleBackColor = True
         '
+        'Lbl_TFOpgave2
+        '
+        Me.Lbl_TFOpgave2.AutoSize = True
+        Me.Lbl_TFOpgave2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_TFOpgave2.Location = New System.Drawing.Point(467, 80)
+        Me.Lbl_TFOpgave2.Name = "Lbl_TFOpgave2"
+        Me.Lbl_TFOpgave2.Size = New System.Drawing.Size(159, 96)
+        Me.Lbl_TFOpgave2.TabIndex = 18
+        Me.Lbl_TFOpgave2.Text = "Opgave 1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Du har følgende vinkelmål:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "a) 54,2 grader" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "b) 135,8 grader" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "c) 256,7 g" &
+    "rader" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "d) 341,7 grader" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'But_opg2d_tjek
+        '
+        Me.But_opg2d_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg2d_tjek.Location = New System.Drawing.Point(612, 281)
+        Me.But_opg2d_tjek.Name = "But_opg2d_tjek"
+        Me.But_opg2d_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg2d_tjek.TabIndex = 30
+        Me.But_opg2d_tjek.Text = "Tjek"
+        Me.But_opg2d_tjek.UseVisualStyleBackColor = True
+        '
+        'But_opg2c_tjek
+        '
+        Me.But_opg2c_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg2c_tjek.Location = New System.Drawing.Point(612, 250)
+        Me.But_opg2c_tjek.Name = "But_opg2c_tjek"
+        Me.But_opg2c_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg2c_tjek.TabIndex = 29
+        Me.But_opg2c_tjek.Text = "Tjek"
+        Me.But_opg2c_tjek.UseVisualStyleBackColor = True
+        '
+        'But_opg2b_tjek
+        '
+        Me.But_opg2b_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg2b_tjek.Location = New System.Drawing.Point(612, 222)
+        Me.But_opg2b_tjek.Name = "But_opg2b_tjek"
+        Me.But_opg2b_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg2b_tjek.TabIndex = 28
+        Me.But_opg2b_tjek.Text = "Tjek"
+        Me.But_opg2b_tjek.UseVisualStyleBackColor = True
+        '
+        'But_opg2a_tjek
+        '
+        Me.But_opg2a_tjek.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.But_opg2a_tjek.Location = New System.Drawing.Point(612, 190)
+        Me.But_opg2a_tjek.Name = "But_opg2a_tjek"
+        Me.But_opg2a_tjek.Size = New System.Drawing.Size(59, 20)
+        Me.But_opg2a_tjek.TabIndex = 27
+        Me.But_opg2a_tjek.Text = "Tjek"
+        Me.But_opg2a_tjek.UseVisualStyleBackColor = True
+        '
+        'Txtbox_opg2d
+        '
+        Me.Txtbox_opg2d.Location = New System.Drawing.Point(492, 281)
+        Me.Txtbox_opg2d.Name = "Txtbox_opg2d"
+        Me.Txtbox_opg2d.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg2d.TabIndex = 26
+        '
+        'Txtbox_opg2c
+        '
+        Me.Txtbox_opg2c.Location = New System.Drawing.Point(492, 251)
+        Me.Txtbox_opg2c.Name = "Txtbox_opg2c"
+        Me.Txtbox_opg2c.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg2c.TabIndex = 25
+        '
+        'Txtbox_opg2b
+        '
+        Me.Txtbox_opg2b.Location = New System.Drawing.Point(492, 222)
+        Me.Txtbox_opg2b.Name = "Txtbox_opg2b"
+        Me.Txtbox_opg2b.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg2b.TabIndex = 24
+        '
+        'Txtbox_opg2a
+        '
+        Me.Txtbox_opg2a.Location = New System.Drawing.Point(493, 190)
+        Me.Txtbox_opg2a.Name = "Txtbox_opg2a"
+        Me.Txtbox_opg2a.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbox_opg2a.TabIndex = 23
+        '
+        'Lbl_opg2d
+        '
+        Me.Lbl_opg2d.AutoSize = True
+        Me.Lbl_opg2d.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg2d.Location = New System.Drawing.Point(467, 281)
+        Me.Lbl_opg2d.Name = "Lbl_opg2d"
+        Me.Lbl_opg2d.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg2d.TabIndex = 22
+        Me.Lbl_opg2d.Text = "d)"
+        '
+        'Lbl_opg2c
+        '
+        Me.Lbl_opg2c.AutoSize = True
+        Me.Lbl_opg2c.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg2c.Location = New System.Drawing.Point(467, 251)
+        Me.Lbl_opg2c.Name = "Lbl_opg2c"
+        Me.Lbl_opg2c.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg2c.TabIndex = 21
+        Me.Lbl_opg2c.Text = "c)"
+        '
+        'Lbl_opg2b
+        '
+        Me.Lbl_opg2b.AutoSize = True
+        Me.Lbl_opg2b.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg2b.Location = New System.Drawing.Point(467, 222)
+        Me.Lbl_opg2b.Name = "Lbl_opg2b"
+        Me.Lbl_opg2b.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg2b.TabIndex = 20
+        Me.Lbl_opg2b.Text = "b)"
+        '
+        'Lbl_opg2a
+        '
+        Me.Lbl_opg2a.AutoSize = True
+        Me.Lbl_opg2a.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_opg2a.Location = New System.Drawing.Point(467, 190)
+        Me.Lbl_opg2a.Name = "Lbl_opg2a"
+        Me.Lbl_opg2a.Size = New System.Drawing.Size(19, 16)
+        Me.Lbl_opg2a.TabIndex = 19
+        Me.Lbl_opg2a.Text = "a)"
+        '
         'Trigonomiske_funktioner
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(816, 447)
+        Me.Controls.Add(Me.But_opg2d_tjek)
+        Me.Controls.Add(Me.But_opg2c_tjek)
+        Me.Controls.Add(Me.But_opg2b_tjek)
+        Me.Controls.Add(Me.But_opg2a_tjek)
+        Me.Controls.Add(Me.Txtbox_opg2d)
+        Me.Controls.Add(Me.Txtbox_opg2c)
+        Me.Controls.Add(Me.Txtbox_opg2b)
+        Me.Controls.Add(Me.Txtbox_opg2a)
+        Me.Controls.Add(Me.Lbl_opg2d)
+        Me.Controls.Add(Me.Lbl_opg2c)
+        Me.Controls.Add(Me.Lbl_opg2b)
+        Me.Controls.Add(Me.Lbl_opg2a)
+        Me.Controls.Add(Me.Lbl_TFOpgave2)
         Me.Controls.Add(Me.But_opg1d_tjek)
         Me.Controls.Add(Me.But_opg1c_tjek)
         Me.Controls.Add(Me.But_opg1b_tjek)
@@ -223,4 +368,17 @@ Partial Class Trigonomiske_funktioner
     Friend WithEvents But_opg1b_tjek As Button
     Friend WithEvents But_opg1c_tjek As Button
     Friend WithEvents But_opg1d_tjek As Button
+    Friend WithEvents Lbl_TFOpgave2 As Label
+    Friend WithEvents But_opg2d_tjek As Button
+    Friend WithEvents But_opg2c_tjek As Button
+    Friend WithEvents But_opg2b_tjek As Button
+    Friend WithEvents But_opg2a_tjek As Button
+    Friend WithEvents Txtbox_opg2d As TextBox
+    Friend WithEvents Txtbox_opg2c As TextBox
+    Friend WithEvents Txtbox_opg2b As TextBox
+    Friend WithEvents Txtbox_opg2a As TextBox
+    Friend WithEvents Lbl_opg2d As Label
+    Friend WithEvents Lbl_opg2c As Label
+    Friend WithEvents Lbl_opg2b As Label
+    Friend WithEvents Lbl_opg2a As Label
 End Class
